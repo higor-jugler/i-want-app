36c1b9497e80a0fded55ddae585b6199eaeec983
Higor Jugler <higorjugler@hotmail.com>
2022-07-26 15:18:42 -0300

    Padronizando endpoint

---
+++ src/EndPoints/Categories/CategoryPost.cs
@@ -0,0 +1,15 @@
+﻿using IWantApp.Infra.Data;
+
+namespace IWantApp.EndPoints.Categories;
+
+public static class CategoryPost
+{
+    public static string Template => "/categories";
+    public static string[] Methods => new string[] { HttpMethod.Post.ToString() };
+    public static Delegate Handle => Action;
+
+    public static IResult Action(CategoryRequest categoryRequest, ApplicationDbContext context)
+    {
+        return Results.Ok("Ok");
+    }
+}
---
+++ src/EndPoints/Categories/CategoryRequest.cs
@@ -0,0 +1,5 @@
+﻿namespace IWantApp.EndPoints.Categories;
+
+public class CategoryRequest
+{
+}
